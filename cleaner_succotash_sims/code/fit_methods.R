e1eb34036ea244578a49f412dc002d7803f12124
David Gerard <gerard.1787@gmail.com>
2016-06-29 14:00:04 -0500

    haven not updated in awhile

---
+++ cleaner_succotash_sims/code/fit_methods.R
@@ -0,0 +1,278 @@
+
+get_auc <- function(predictor, response) {
+    pROC::roc(response = response, predictor = predictor)$auc
+}
+
+#' Fit all competitors and return betahat, lfdr, and pi0hat.
+#'
+#'
+#' This function won't work yet if ofinterest is not equal to ncol(X).
+#'
+#' @param Y An n by p matrix of numerics. The normalized count data.
+#' @param X An n by k matrix. The covariates.
+#' @param num_sv A positive integer. The number of hidden confounders.
+#' @param ofinterest A positive integer. The column of X whose
+#'     betahats to return. Right now only works if 2.
+#'
+#'
+#'
+fit_all_competitors <- function(Y, X, num_sv, ofinterest = ncol(X), control_genes = NULL) {
+
+    if (ofinterest != 2) {
+        stop("ofinterest not supported unless equal to 2")
+    }
+
+    pi0hat_vec <- c()
+
+
+    ## OLS-----------------------------------------------------------------
+    ols_fit <- get_ols(log_counts = t(Y), condition = X[, 2])
+    ols_out <- fit_freq_methods(out_obj = ols_fit)
+
+    betahat_df <- data.frame(ols = ols_fit$betahat)
+    lfdr_df    <- data.frame(ols = ols_out$q_storey$lfdr)
+    pi0hat_vec <- c(pi0hat_vec, ols_out$q_storey$pi0)
+
+    ## OLS + ASH ---------------------------------------------------------
+    ash_ols <- ashr::ash(betahat = ols_fit$betahat, sebetahat = ols_fit$sebetahat)
+    betahat_df$ols_ash <- ash_ols$PosteriorMean
+    lfdr_df$ols_ash <- ash_ols$lfdr
+    pi0hat_vec <- c(pi0hat_vec, ash_ols$fitted.g$pi[1])
+
+    ## SUCCOTASH ---------------------------------------------------------
+    succ_out <- succotashr::succotash(Y = Y, X = X, k = num_sv,
+                                      fa_method = "pca", num_em_runs = 3,
+                                      optmethod = "em",
+                                      two_step = TRUE,
+                                      var_scale_init_type = "null_mle",
+                                      z_init_type = "null_mle")
+
+    betahat_df$succotash <- succ_out$betahat
+    lfdr_df$succotash    <- succ_out$lfdr
+    pi0hat_vec           <- c(pi0hat_vec, succ_out$pi0)
+    succ_lfsr            <- data.frame(normal = succ_out$lfsr)
+
+    ## succotash-t---------------------------------------------------------
+    ## succ_out_t <- succotashr::succotash(Y = Y, X = X, k = num_sv,
+    ##                                   fa_method = "pca", num_em_runs = 3,
+    ##                                   likelihood = "t", mix_type = "uniform")
+
+    ## betahat_df$succotash_t <- succ_out_t$betahat
+    ## lfdr_df$succotash_t    <- succ_out_t$lfdr
+    ## pi0hat_vec             <- c(pi0hat_vec, succ_out_t$pi0)
+
+    ## lfsr data frame for succotash -------------------------------------
+    ## succ_lfsr <- data.frame(normal = succ_out$lfsr, t = succ_out_t$lfsr)
+
+
+    ## RR CATE-----------------------------------------------------------
+    ## error if only use one surrogate variable.
+    num_sv_cate <- max(num_sv, 2)
+    if (nrow(Y) - num_sv_cate - ncol(X) <= 0) {
+        num_sv_cate <- 0
+    }
+    cate_rr <- cate::cate(~Treatment, Y = Y,
+                          X.data = data.frame(Treatment = X[, 2]),
+                          r = num_sv_cate, fa.method = "pc", adj.method = "rr",
+                          calibrate = FALSE)
+    cate_rr_out           <- list()
+    cate_rr_out$betahat   <- cate_rr$beta
+    cate_rr_out$sebetahat <- sqrt(cate_rr$beta.cov.row * cate_rr$beta.cov.col) / sqrt(nrow(X))
+    cate_rr_out$pvalue    <- cate_rr$beta.p.value
+    cate_qv               <- fit_freq_methods(out_obj = cate_rr_out)
+
+    betahat_df$cate_rr <- cate_rr_out$betahat
+    lfdr_df$cate_rr    <- cate_qv$q_storey$lfdr
+    pi0hat_vec         <- c(pi0hat_vec, cate_qv$q_storey$pi0)
+
+
+    ## RR CATE + ASH ----------------------------------------------------
+    ## ash_rr_cate <- fit_ash(cate_rr_out)
+    ## betahat_df$cate_rr_ash <- ash_rr_cate$PosteriorMean
+    ## lfdr_df$cate_rr_ash <- ash_rr_cate$lfdr
+    ## pi0hat_vec <- c(pi0hat_vec, ash_rr_cate$fitted.g$pi[1])
+
+    ## LEAPP Sparse-------------------------------------------------------
+    ## Sometimes stalls when sparse = TRUE
+    ## trash2 <- tryCatch({
+    ##     trash <- R.utils::evalWithTimeout({
+    ##         leapp_sparse <- leapp::leapp(dat = t(Y), pred.prim = X[, ofinterest],
+    ##                                      pred.covar = X[, -ofinterest],
+    ##                                      num.fac = num_sv)
+    ##         leapp_sparse_out         <- list()
+    ##         leapp_sparse_out$betahat <- leapp_sparse$gamma
+    ##         leapp_sparse_out$pvalue  <- leapp_sparse$p
+    ##         leapp_sparse_qv          <- fit_freq_methods(out_obj = leapp_sparse_out)
+
+    ##         betahat_df$leapp_sparse <- leapp_sparse_out$betahat
+    ##         lfdr_df$leapp_sparse    <- leapp_sparse_qv$q_storey$lfdr
+    ##         ##pi0hat_vec            <- c(pi0hat_vec, leapp_sparse_qv$q_storey$pi0)
+    ##         pi0hat_vec              <- c(pi0hat_vec,
+    ##                                      mean(abs(leapp_sparse_out$betahat) <= 10 ^ -10))
+    ##         TRUE
+    ##     },
+    ##     timeout = 120,
+    ##     onTimeout = "silent")
+    ## },
+    ## error = function(e){NULL})
+    ## if(is.null(trash2)) {
+    ##     betahat_df$leapp_sparse <- rep(NA, length = ncol(Y))
+    ##     lfdr_df$leapp_sparse    <- rep(NA, length = ncol(Y))
+    ##     pi0hat_vec              <- c(pi0hat_vec, NA)
+    ## } else if (is.null(trash)) {
+    ##     betahat_df$leapp_sparse <- rep(NA, length = ncol(Y))
+    ##     lfdr_df$leapp_sparse    <- rep(NA, length = ncol(Y))
+    ##     pi0hat_vec              <- c(pi0hat_vec, NA)
+    ## }
+
+
+    ## LEAPP Ridge--------------------------------------------------------
+    ## Sometimes gives p-values that are all exactly zero and throws an error.
+    ## trash <- tryCatch({
+    ##     leapp_ridge <- leapp::leapp(dat = t(Y), pred.prim = X[, ofinterest],
+    ##                                 pred.covar = X[, -ofinterest],
+    ##                                 num.fac = num_sv, method = "hard", sparse = FALSE)
+    ##     leapp_ridge_out         <- list()
+    ##     leapp_ridge_out$betahat <- leapp_ridge$gamma
+    ##     leapp_ridge_out$pvalue  <- leapp_ridge$p
+    ##     leapp_ridge_qv          <- fit_freq_methods(out_obj = leapp_ridge_out)
+
+    ##     betahat_df$leapp_ridge <- leapp_ridge_out$betahat
+    ##     lfdr_df$leapp_ridge    <- leapp_ridge_qv$q_storey$lfdr
+    ##     pi0hat_vec             <- c(pi0hat_vec, leapp_ridge_qv$q_storey$pi0)
+    ##     TRUE
+    ## },
+    ## error = function(e){NULL})
+    ## if(is.null(trash)) {
+    ##     betahat_df$leapp_ridge <- rep(NA, length = ncol(Y))
+    ##     lfdr_df$leapp_ridge    <- rep(NA, length = ncol(Y))
+    ##     pi0hat_vec             <- c(pi0hat_vec, NA)
+    ## }
+
+
+    ## SVA----------------------------------------------------------------
+    trash      <- capture.output(sva_out <- sva::sva(dat = t(Y), mod = X, n.sv = num_sv))
+    X.sv       <- cbind(X, sva_out$sv)
+    sva_ols    <- get_ols(log_counts = t(Y), condition = X.sv[, -1])
+    sva_ols_qv <- fit_freq_methods(out_obj = sva_ols)
+
+    betahat_df$sva_ols <- sva_ols$betahat
+    lfdr_df$sva_ols    <- sva_ols_qv$q_storey$lfdr
+    pi0hat_vec         <- c(pi0hat_vec, sva_ols_qv$q_storey$pi0)
+
+    ## SVA + MAD + ASH
+    ashsva_out             <- ashr::ash_sva_mad(Y = Y, X = X, k = num_sv, limmashrink = TRUE)
+    betahat_df$ash_sva_mad <- ashsva_out$PosteriorMean
+    lfdr_df$ash_sva_mad    <- ashsva_out$lfdr
+    pi0hat_vec             <- c(pi0hat_vec, ashsva_out$fitted.g$pi[1])
+
+    ## VOOM + LIMMA + EBAYES + MAD + ASH
+    vlema_out        <- ashr::vlema(Y = 2 ^ Y - 1, X = X)
+    betahat_df$vlema <- vlema_out$PosteriorMean
+    lfdr_df$vlema    <- vlema_out$lfdr
+    pi0hat_vec       <- c(pi0hat_vec, vlema_out$fitted.g$pi[1])
+
+    ## Negative control methods--------------------------------------------
+    if (!is.null(control_genes)) {
+
+        ## RUVASH ---------------------------------------------------------
+        ruvash_out <- ashr::ash_ruv(Y = Y, X = X, k = num_sv, ctl = as.logical(control_genes),
+                                    limmashrink = TRUE)
+        betahat_df$ruvash <- ruvash_out$PosteriorMean
+        lfdr_df$ruvash <- ruvash_out$lfdr
+        pi0hat_vec <- c(pi0hat_vec, ruvash_out$fitted.g$pi[1])
+
+        ## Inflated RUV4 with GLS ----------------------------------------
+        qruv4inflate            <- qvalue::qvalue(p = ruvash_out$ruv$pvalues)
+        betahat_df$ruv4_inflate <- ruvash_out$ruv$betahat
+        lfdr_df$ruv4_inflate    <- qruv4inflate$lfdr
+        pi0hat_vec              <- c(pi0hat_vec, qruv4inflate$pi0)
+
+        ashruv4_out             <- ashr::ash_ruv_mad(Y = Y, X = X, k = num_sv, limmashrink = TRUE,
+                                                     ctl = as.logical(control_genes),
+                                                     ruv_type = "ruv4")
+        betahat_df$ash_ruv4_mad <- ashruv4_out$PosteriorMean
+        lfdr_df$ash_ruv4_mad    <- ashruv4_out$lfdr
+        pi0hat_vec              <- c(pi0hat_vec, ashruv4_out$fitted.g$pi[1])
+
+        ashruv2_out             <- ashr::ash_ruv_mad(Y = Y, X = X, k = num_sv, limmashrink = TRUE,
+                                                     ctl = as.logical(control_genes),
+                                                     ruv_type = "ruv2")
+        betahat_df$ash_ruv2_mad <- ashruv2_out$PosteriorMean
+        lfdr_df$ash_ruv2_mad    <- ashruv2_out$lfdr
+        pi0hat_vec              <- c(pi0hat_vec, ashruv2_out$fitted.g$pi[1])
+
+
+        ## Negative Control CATE --------------------------------------
+        cate_nc <- cate::cate(~Treatment, Y = Y,
+                              X.data = data.frame(Treatment = X[, 2]),
+                              r = num_sv_cate, fa.method = "pc", adj.method = "nc",
+                              nc = as.logical(control_genes), calibrate = FALSE)
+        cate_nc_out           <- list()
+        cate_nc_out$betahat   <- cate_nc$beta
+        cate_nc_out$sebetahat <- sqrt(cate_nc$beta.cov.row * cate_nc$beta.cov.col) /
+            sqrt(nrow(X))
+        cate_nc_out$pvalue    <- cate_nc$beta.p.value
+        cate_nc_qv            <- fit_freq_methods(out_obj = cate_nc_out)
+
+        betahat_df$cate_nc <- cate_nc_out$betahat
+        lfdr_df$cate_nc    <- cate_nc_qv$q_storey$lfdr
+        pi0hat_vec         <- c(pi0hat_vec, cate_nc_qv$q_storey$pi0)
+
+        ## NC CATE + ASH
+        ## ash_nc_cate <- fit_ash(cate_nc_out)
+        ## betahat_df$cate_nc_ash <- ash_nc_cate$PosteriorMean
+        ## lfdr_df$cate_nc_ash <- ash_nc_cate$lfdr
+        ## pi0hat_vec <- c(pi0hat_vec, ash_nc_cate$fitted.g$pi[1])
+
+        ## Exact same thing as RUV2
+        ## ## Negative Control SVA ---------------------------------------------
+        ## trash <- capture.output(sva_nc_out <- sva::sva(dat = t(Y), mod = X, n.sv = num_sv,
+        ##                                             controls = control_genes))
+        ## X.sv_nc <- cbind(X, sva_nc_out$sv)
+        ## sva_nc_ols <- get_ols(log_counts = t(Y), condition = X.sv_nc[, -1])
+        ## sva_nc_ols_qv <- fit_freq_methods(out_obj = sva_nc_ols)
+
+        ## betahat_df$sva_nc_ols <- sva_nc_ols$betahat
+        ## lfdr_df$sva_nc_ols <- sva_nc_ols_qv$q_storey$lfdr
+        ## pi0hat_vec <- c(pi0hat_vec, sva_nc_ols_qv$q_storey$pi0)
+
+
+        ## RUV2-------------------------------------------------------
+        ruv_ruv2 <- ruv::RUV2(Y = Y, X = as.matrix(X[, ofinterest]),
+                              ctl = as.logical(control_genes),
+                              k = num_sv, Z = as.matrix(X[, -ofinterest]))
+        ruv_ruv2_list         <- list()
+        ruv_ruv2_list$betahat <- c(ruv_ruv2$betahat)
+        ruv_ruv2_list$pvalue  <- c(ruv_ruv2$p)
+        ruv_ruv2_list$df      <- rep(ruv_ruv2$df, length = ncol(Y))
+        ruv2_qv               <- fit_freq_methods(out_obj = ruv_ruv2_list)
+
+        betahat_df$ruv2 <- ruv_ruv2_list$betahat
+        lfdr_df$ruv2    <- ruv2_qv$q_storey$lfdr
+        pi0hat_vec      <- c(pi0hat_vec, ruv2_qv$q_storey$pi0)
+
+        ## RUV4 -------------------------------------------------------
+        ruv_ruv4 <- ruv::RUV4(Y = Y, X = as.matrix(X[, ofinterest]),
+                              ctl = as.logical(control_genes),
+                              k = num_sv, Z = as.matrix(X[, -ofinterest]))
+        ruv_ruv4_list         <- list()
+        ruv_ruv4_list$betahat <- c(ruv_ruv4$betahat)
+        ruv_ruv4_list$pvalue  <- c(ruv_ruv4$p)
+        ruv_ruv4_list$df      <- rep(ruv_ruv4$df, length = ncol(Y))
+        ruv4_qv               <- fit_freq_methods(out_obj = ruv_ruv4_list)
+
+        betahat_df$ruv4 <- ruv_ruv4_list$betahat
+        lfdr_df$ruv4    <- ruv4_qv$q_storey$lfdr
+        pi0hat_vec      <- c(pi0hat_vec, ruv4_qv$q_storey$pi0)
+
+        ## RUV4 + ASH -------------------------------------------------
+        ruv4sebetahat <- sqrt(ruv_ruv4$sigma2 * ruv_ruv4$multiplier)
+        ashr_ruv4_out <- ashr::ash(betahat = ruv_ruv4$betahat, ruv4sebetahat)
+        betahat_df$ruv4ThenAsh <- ashr_ruv4_out$PosteriorMean
+        lfdr_df$ruv4ThenAsh <- ashr_ruv4_out$lfdr
+        pi0hat_vec <- c(pi0hat_vec, ashr_ruv4_out$fitted.g$pi[1])
+    }
+
+    return(list(betahat_df = betahat_df, lfdr_df = lfdr_df, pi0hat_vec = pi0hat_vec))
+}
---
+++ cleaner_succotash_sims/succotash_diff_init_onestep/muscle_succ_diff_init_onestep.R
@@ -0,0 +1,124 @@
+library(Rmpi)
+library(snow)
+
+one_rep <- function(new_params, current_params) {
+    source("../code/datamaker_only_counts.R")
+    args_val <- append(current_params, new_params)
+    set.seed(new_params$current_seed)
+
+    d_out <- datamaker_counts_only(args_val)
+
+    which_null <- d_out$meta$null
+
+    half_null <- which_null
+    half_null[half_null == 1][sample(1:sum(which_null), size = sum(which_null) / 2)] <- 0
+
+    beta_true <- rep(0, length = args_val$Ngene)
+    beta_true[!which_null] <- d_out$meta$true_log2foldchange
+
+    X <- as.matrix(model.matrix(~d_out$input$condition))
+    colnames(X) <- c("Intercept", "Treatment")
+    Y <- t(log2(as.matrix(d_out$input$counts + 1)))
+
+    num_sv <- sva::num.sv(t(Y), mod = X, method = "be")
+
+    start.time <- proc.time()
+    succ_nullmle <- succotashr::succotash(Y = Y, X = X, k = num_sv,
+                                          two_step = FALSE,
+                                          z_init_type = "null_mle",
+                                          var_scale_init_type = "null_mle",
+                                          optmethod = "em")
+    succ_random <- succotashr::succotash(Y = Y, X = X, k = num_sv,
+                                         two_step = FALSE,
+                                         z_init_type = "random",
+                                         var_scale_init_type = "random",
+                                         optmethod = "em")
+    tot.time <- proc.time() - start.time
+
+    postmean_df <- data.frame(succ_nullmle = succ_nullmle$betahat,
+                              succ_random = succ_random$betahat)
+    pi0vec      <- c(succ_nullmle = succ_nullmle$pi0,
+                     succ_random = succ_random$pi0)
+    scale_val_vec <-  c(succ_nullmle = succ_nullmle$scale_val,
+                        succ_random = succ_random$scale_val)
+    llike_vec <-  c(succ_nullmle = succ_nullmle$llike,
+                        succ_random = succ_random$llike)
+    null_llike_vec <-  c(succ_nullmle = succ_nullmle$null_llike,
+                        succ_random = succ_random$null_llike)
+    lfdr_df     <- data.frame(succ_nullmle = succ_nullmle$lfdr,
+                              succ_random = succ_random$lfdr)
+    nmeth <- length(pi0vec)
+
+    mse <- colMeans((postmean_df - beta_true) ^ 2)
+    auc <- rep(NA, nmeth)
+    if(args_val$nullpi < 1) {
+        for (index in 1:nmeth) {
+            auc[index] <- pROC::roc(predictor = lfdr_df[, index], response = which_null)$auc
+        }
+    }
+
+    return_vec <- c(pi0vec, mse, auc, scale_val_vec, llike_vec, null_llike_vec)
+
+    return(return_vec)
+}
+
+itermax <- 100
+
+## these change
+Nsamp_seq  <- c(5, 10, 20)
+nullpi_seq <- c(0.5, 0.9, 1)
+
+par_vals <- expand.grid(list(1:itermax, Nsamp_seq, nullpi_seq))
+colnames(par_vals) <- c("current_seed", "Nsamp", "nullpi")
+par_vals$poisthin <- TRUE
+par_vals$poisthin[abs(par_vals$nullpi - 1) < 10 ^ -10] <- FALSE
+
+par_list <- list()
+for (list_index in 1:nrow(par_vals)) {
+    par_list[[list_index]] <- list()
+    for (inner_list_index in 1:ncol(par_vals)) {
+        par_list[[list_index]][[inner_list_index]] <- par_vals[list_index, inner_list_index]
+        names(par_list[[list_index]])[inner_list_index] <- colnames(par_vals)[inner_list_index]
+    }
+}
+
+## these do not change
+args_val              <- list()
+args_val$log2foldsd   <- 1
+args_val$tissue       <- "muscle"
+args_val$path         <- "../../../data/gtex_tissue_gene_reads/"
+args_val$Ngene        <- 1000
+args_val$log2foldmean <- 0
+args_val$skip_gene    <- 0
+
+## ## If on your own computer, use this
+library(parallel)
+cl <- makeCluster(detectCores()-1)
+sout <- t(parSapply(cl = cl, par_list, FUN = one_rep, current_params = args_val))
+stopCluster(cl)
+
+## ## on RCC, use this
+## np <- mpi.universe.size() - 1
+## cluster <- makeMPIcluster(np)
+## sout <- t(snow::parSapply(cl = cluster, X = par_list, FUN = one_rep, current_params = args_val))
+## stopCluster(cluster)
+## mpi.exit()
+
+
+save(sout, file = "sout_succ_diff_init.Rd")
+
+colnames(sout) <- rep(c("succ_null_mle", "succ_random"), times = 6)
+
+pi0_mat <- cbind(par_vals, sout[, 1:2])
+mse_mat <- cbind(par_vals, sout[, 3:4])
+auc_mat <- cbind(par_vals, sout[, 5:6])
+scale_val_mat <- cbind(par_vals, sout[, 7:8])
+llike_mat <- cbind(par_vals, sout[, 9:10])
+null_llike_mat <- cbind(par_vals, sout[, 11:12])
+
+write.csv(pi0_mat, file = "pi0_ruvash_alpha1.csv", row.names = FALSE)
+write.csv(mse_mat, file = "mse_ruvash_alpha1.csv", row.names = FALSE)
+write.csv(auc_mat, file = "auc_ruvash_alpha1.csv", row.names = FALSE)
+write.csv(scale_val_mat, file = "scale_val_ruvash_alpha1.csv", row.names = FALSE)
+write.csv(llike_mat, file = "llike_ruvash_alpha1.csv", row.names = FALSE)
+write.csv(null_llike_mat, file = "null_llike_ruvash_alpha1.csv", row.names = FALSE)
